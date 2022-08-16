066c11d5e84d7139dc5c09030c6500dca507594c
letrong29 <letrong2929Gmail.com>
2022-08-16 08:38:06 +0700

    fix

---
+++ ss1_overview_spring_mvc/exercise/dictionary/src/main/java/com/example/controller/DictionaryController.java
@@ -0,0 +1,33 @@
+package com.example.controller;
+
+import com.example.service.IDictionaryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class DictionaryController {
+    @Autowired
+    private IDictionaryService iDictionaryService;
+
+    @GetMapping("/")
+    public String home() {
+        return "/index";
+    }
+
+    @GetMapping("/translate")
+    public String goTranslate() {
+        return "/translate";
+    }
+
+    @PostMapping("/trans")
+    public String trans(@RequestParam String keySearch, Model model) {
+        String result = iDictionaryService.trans(keySearch);
+        model.addAttribute("keySearch", keySearch);
+        model.addAttribute("result", result);
+        return "/translate";
+    }
+}
